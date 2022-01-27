2a8db58546db1ce1e0b8853dd183f9a859422185
appcpf <antoniop94@gmail.com>
2022-01-27 11:13:43 -0400

    Programming assignment number 2. R code to create the functions: makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming
+## --------------------------------
+## Week 3: Programming Assignment 2
+## --------------------------------
 
-## Write a short comment describing this function
+## ----------------------------------------------------------------
+## The following function will store the inverse of a matrix.
+## I have placed the step-by-step description inside each function.
+## ----------------------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # First create variable (empty cache)
+        InverseM <- NULL
+        
+        # Put the value of the matrix
+        set <- function(y){
+                # The <<- operator allows "set" to instantiate x outside of the function's environment
+                x <<- y
+                InverseM <<- NULL
+        }
+        
+        # Get matrix value
+        get <- function() x # returns x
+        
+        # Get the value of the inverse and store it in "inverse"
+        setInverseM <- function(inv) InverseM <<- inv
+        
+        # Retrieve the value of the inverse
+        getInverseM <- function() InverseM 
+        
+        # List functions so you can call them later
+        list(set = set, get = get,
+             setInverseM = setInverseM,
+             getInverseM = getInverseM)
 }
 
 
-## Write a short comment describing this function
+## ----------------------------------------------------------------
+## The following function will get the inverse matrix in cache.
+## I have placed the step-by-step description inside each function.
+## ----------------------------------------------------------------
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Get the return value of the Inverse matrix
+        InverseM <- x$getInverseM()
+        
+        # Condition to evaluate whether or not there is a value in the cache
+        # If cache value is NOT null, get cache...
+        if(!is.null(InverseM)){
+                message("Getting Data in Cache...")
+                return(InverseM)
+        }
+        # ...If IS null, then get the inverse (with solve()) and cache it
+        value <- x$get()
+        InverseM <- solve(value,...)
+        x$setInverseM(InverseM)
+        InverseM #returns the Inverse matrix
 }
